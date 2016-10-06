a3543799fe23ffc6b5df273e9c82ff7f925b6dc6
bb118314 <roflwhy@gmail.com>
2016-10-06 11:28:25 -0500

    Add files via upload

---
+++ Zelda3/Zelda3/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.C1 = New System.Windows.Forms.PictureBox()
+        Me.C2 = New System.Windows.Forms.PictureBox()
+        Me.C3 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C5 = New System.Windows.Forms.PictureBox()
+        Me.C6 = New System.Windows.Forms.PictureBox()
+        Me.C7 = New System.Windows.Forms.PictureBox()
+        Me.C4 = New System.Windows.Forms.PictureBox()
+        CType(Me.C1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'C1
+        '
+        Me.C1.Image = CType(resources.GetObject("C1.Image"), System.Drawing.Image)
+        Me.C1.Location = New System.Drawing.Point(27, 53)
+        Me.C1.Name = "C1"
+        Me.C1.Size = New System.Drawing.Size(81, 97)
+        Me.C1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C1.TabIndex = 0
+        Me.C1.TabStop = False
+        '
+        'C2
+        '
+        Me.C2.Image = CType(resources.GetObject("C2.Image"), System.Drawing.Image)
+        Me.C2.Location = New System.Drawing.Point(27, 53)
+        Me.C2.Name = "C2"
+        Me.C2.Size = New System.Drawing.Size(81, 97)
+        Me.C2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C2.TabIndex = 1
+        Me.C2.TabStop = False
+        '
+        'C3
+        '
+        Me.C3.Image = CType(resources.GetObject("C3.Image"), System.Drawing.Image)
+        Me.C3.Location = New System.Drawing.Point(27, 53)
+        Me.C3.Name = "C3"
+        Me.C3.Size = New System.Drawing.Size(81, 97)
+        Me.C3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C3.TabIndex = 3
+        Me.C3.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'C5
+        '
+        Me.C5.Image = CType(resources.GetObject("C5.Image"), System.Drawing.Image)
+        Me.C5.Location = New System.Drawing.Point(27, 53)
+        Me.C5.Name = "C5"
+        Me.C5.Size = New System.Drawing.Size(81, 97)
+        Me.C5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C5.TabIndex = 4
+        Me.C5.TabStop = False
+        '
+        'C6
+        '
+        Me.C6.Image = CType(resources.GetObject("C6.Image"), System.Drawing.Image)
+        Me.C6.Location = New System.Drawing.Point(27, 53)
+        Me.C6.Name = "C6"
+        Me.C6.Size = New System.Drawing.Size(81, 97)
+        Me.C6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C6.TabIndex = 5
+        Me.C6.TabStop = False
+        '
+        'C7
+        '
+        Me.C7.Image = CType(resources.GetObject("C7.Image"), System.Drawing.Image)
+        Me.C7.Location = New System.Drawing.Point(27, 53)
+        Me.C7.Name = "C7"
+        Me.C7.Size = New System.Drawing.Size(81, 97)
+        Me.C7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C7.TabIndex = 6
+        Me.C7.TabStop = False
+        '
+        'C4
+        '
+        Me.C4.Image = CType(resources.GetObject("C4.Image"), System.Drawing.Image)
+        Me.C4.Location = New System.Drawing.Point(27, 53)
+        Me.C4.Name = "C4"
+        Me.C4.Size = New System.Drawing.Size(81, 97)
+        Me.C4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.C4.TabIndex = 7
+        Me.C4.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(551, 272)
+        Me.Controls.Add(Me.C4)
+        Me.Controls.Add(Me.C7)
+        Me.Controls.Add(Me.C6)
+        Me.Controls.Add(Me.C5)
+        Me.Controls.Add(Me.C3)
+        Me.Controls.Add(Me.C2)
+        Me.Controls.Add(Me.C1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.C1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents C1 As System.Windows.Forms.PictureBox
+    Friend WithEvents C2 As System.Windows.Forms.PictureBox
+    Friend WithEvents C3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents C5 As System.Windows.Forms.PictureBox
+    Friend WithEvents C6 As System.Windows.Forms.PictureBox
+    Friend WithEvents C7 As System.Windows.Forms.PictureBox
+    Friend WithEvents C4 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Zelda3/Zelda3/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Up
+                Timer1.Enabled = True
+                C1.Top -= 7
+                C2.Top -= 7
+                C3.Top -= 7
+                C4.Top -= 7
+                C5.Top -= 7
+                C6.Top -= 7
+                C7.Top -= 7
+
+            Case Keys.Down
+                Timer1.Enabled = True
+                C1.Top += 7
+                C2.Top += 7
+                C3.Top += 7
+                C4.Top += 7
+                C5.Top += 7
+                C6.Top += 7
+                C7.Top += 7
+
+            Case Keys.Left
+                Timer1.Enabled = True
+                C1.Left -= 7
+                C2.Left -= 7
+                C3.Left -= 7
+                C4.Left -= 7
+                C5.Left -= 7
+                C6.Left -= 7
+                C7.Left -= 7
+
+            Case Keys.Right
+                Timer1.Enabled = True
+                C1.Left += 7
+                C2.Left += 7
+                C3.Left += 7
+                C4.Left += 7
+                C5.Left += 7
+                C6.Left += 7
+                C7.Left += 7
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If C1.Visible = True Then
+            C1.Visible = False
+            C2.Visible = True
+            C4.Visible = False
+            C6.Visible = False
+            C7.Visible = False
+            C5.Visible = False
+            C3.Visible = False
+
+        ElseIf C2.Visible = True Then
+            C2.Visible = False
+            C3.Visible = True
+            C4.Visible = False
+            C5.Visible = False
+            C6.Visible = False
+            C7.Visible = False
+        ElseIf C3.Visible = True Then
+            C1.Visible = False
+            C2.Visible = False
+            C3.Visible = False
+            C4.Visible = True
+            C5.Visible = False
+            C6.Visible = False
+            C7.Visible = False
+        ElseIf C4.Visible = True Then
+            C1.Visible = False
+            C2.Visible = False
+            C3.Visible = False
+            C4.Visible = False
+            C5.Visible = True
+            C6.Visible = False
+            C7.Visible = False
+        ElseIf C5.Visible = True Then
+            C1.Visible = False
+            C2.Visible = False
+            C3.Visible = False
+            C4.Visible = False
+            C5.Visible = False
+            C6.Visible = True
+            C7.Visible = False
+        ElseIf C6.Visible = True Then
+            C6.Visible = False
+            C7.Visible = True
+            C1.Visible = False
+            C2.Visible = False
+            C3.Visible = False
+            C4.Visible = False
+            C5.Visible = False
+
+        End If
+
+    End Sub
+
+    Private Sub C6_Click(sender As Object, e As EventArgs) Handles C6.Click
+
+    End Sub
+End Class
